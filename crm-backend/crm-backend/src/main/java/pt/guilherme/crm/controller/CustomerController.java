3029ae23591b3bb8e4ca89e54ee0b8ad92e7c364
guilhermeSilva96 <139381851+guilhermeSilva96@users.noreply.github.com>
2024-01-17 03:52:05 +0000

    Update CustomerController.java

--- crm-backend/crm-backend/src/main/java/pt/guilherme/crm/controller/CustomerController.java
+++ crm-backend/crm-backend/src/main/java/pt/guilherme/crm/controller/CustomerController.java
@@ -17,28 +17,28 @@ public class CustomerController {
 
     private CustomerService customerService;
 
-    // Build add Employee Rest API
+   
 @PostMapping
     public ResponseEntity<CustomerDto> createCustomer(@RequestBody CustomerDto customerDto){
         CustomerDto savedCustomer = customerService.createCustomer(customerDto);
         return  new ResponseEntity<>(savedCustomer, HttpStatus.CREATED);
     }
 
-    // Build Get Customer Rest API
+  
 @GetMapping("{id}")
     public ResponseEntity<CustomerDto> getCustomerById(@PathVariable("id") Long customerId){
         CustomerDto customerDto = customerService.getCustomerById(customerId);
         return ResponseEntity.ok(customerDto);
     }
 
-    // Build Get All Customers List Rest API
+    
 @GetMapping
     public ResponseEntity<List<CustomerDto>> getAllCustomers(){
     List<CustomerDto> customers = customerService.getAllCustomers();
     return ResponseEntity.ok(customers);
     }
 
-    // Build Update Customer Rest API
+   
 @PutMapping("{id}")
     public ResponseEntity<CustomerDto> updateCustomer(@PathVariable("id") Long customerId,
                                                       @RequestBody CustomerDto updatedCustomer){
@@ -46,7 +46,7 @@ public class CustomerController {
         return ResponseEntity.ok(customerDto);
 
     }
-// Build delete customer Rest Api
+
 
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteCustomer(@PathVariable("id") Long customerId){
